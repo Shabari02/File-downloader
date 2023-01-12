9409661ffe0447365e898f376f847e8fd3c21718
Shabari02 <lkshabarinathan02@gmail.com>
2023-01-12 17:25:56 +0530

    Header  size modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <h1 className="my-6 text-5xl font-extrabold leading-none tracking-tight text-gray-700 md:text-5xl lg:text-6xl dark:text-white uppercase">File <mark className="px-2 text-white  rounded bg-sec">Downloader</mark></h1>
+            <h1 className="my-6 text-4xl font-extrabold leading-none tracking-tight text-gray-700 md:text-5xl lg:text-6xl dark:text-white uppercase">File <mark className="px-2 text-white  rounded bg-sec">Downloader</mark></h1>
             <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400 text-center">Paste url of image or pdf to download</p>
         </div>
     );
